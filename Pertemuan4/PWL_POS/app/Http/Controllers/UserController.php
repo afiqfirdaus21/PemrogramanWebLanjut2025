d822dc57834d6f8db6160c758b9769e3123128fb
Afiq Firdaus <fafiq8445@gmail.com>
2025-03-11 06:21:03 +0700

    Praktikum-2.1

--- Pertemuan4/PWL_POS/app/Http/Controllers/UserController.php
+++ Pertemuan4/PWL_POS/app/Http/Controllers/UserController.php
@@ -9,19 +9,10 @@ class UserController extends Controller
 {
     public function index()
     {
-        $existinguser = UserModel::where('username', 'manager_lima')->first();
-        if(!$existinguser){
-            $data = [
-                'level_id' => 2,
-                'username' => 'manager_tiga',
-                'nama' => 'Manager 3',
-                'password' => Hash::make('12345')
-            ];
-    
-            UserModel::create($data);
-        }
+        $user = UserModel::findOr(20, ['username', 'nama'], function (){
+            abort(404);
+        });
 
-        $user = UserModel::all();
         return view('user', ['data' => $user]);
     }
 }
