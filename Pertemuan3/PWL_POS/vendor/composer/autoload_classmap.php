5f9e5436a26c86ee5bf2f0571052943785cfcf03
Afiq Firdaus <fafiq8445@gmail.com>
2025-03-11 01:43:25 +0700

    SEEDER

--- Pertemuan3/PWL_POS/vendor/composer/autoload_classmap.php
+++ Pertemuan3/PWL_POS/vendor/composer/autoload_classmap.php
@@ -137,6 +137,8 @@ return array(
     'Cron\\MonthField' => $vendorDir . '/dragonmantank/cron-expression/src/Cron/MonthField.php',
     'Database\\Factories\\UserFactory' => $baseDir . '/database/factories/UserFactory.php',
     'Database\\Seeders\\DatabaseSeeder' => $baseDir . '/database/seeders/DatabaseSeeder.php',
+    'Database\\Seeders\\LevelSeeder' => $baseDir . '/database/seeders/LevelSeeder.php',
+    'Database\\Seeders\\UseSeeder' => $baseDir . '/database/seeders/UseSeeder.php',
     'DateError' => $vendorDir . '/symfony/polyfill-php83/Resources/stubs/DateError.php',
     'DateException' => $vendorDir . '/symfony/polyfill-php83/Resources/stubs/DateException.php',
     'DateInvalidOperationException' => $vendorDir . '/symfony/polyfill-php83/Resources/stubs/DateInvalidOperationException.php',
