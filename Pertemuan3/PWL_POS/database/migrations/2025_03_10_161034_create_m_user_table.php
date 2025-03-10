ccfea5b23d8c0d5727051258924ed7f6f5aace25
Afiq Firdaus <fafiq8445@gmail.com>
2025-03-10 23:31:36 +0700

    Perbaikan Error

--- Pertemuan3/PWL_POS/database/migrations/2025_03_10_161034_create_m_user_table.php
+++ Pertemuan3/PWL_POS/database/migrations/2025_03_10_161034_create_m_user_table.php
@@ -19,7 +19,6 @@ return new class extends Migration
             $table->string('password');
             $table->timestamps();
 
-            $table->foreign('level_id')->references('level_id')->on('m_level');
         });
     }
 
